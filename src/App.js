71592133f3d809281b6f75f964638eab9d16d137
Almog <almo321@gmail.com>
2023-03-31 13:30:21 +0300

    skeleton build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { Users , Movies } from './Data';
+import { useState } from 'react';
+import Landing from './components/Landing';
+import Catalog from './components/Catalog/Catalog';
+import MovieDetail from './components/MovieDetail';
+import Navbar from './components/Header/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [users, setUsers] = useState(Users);
+  const [movies, setMovies] = useState(Movies);
+
+  const getUserData = (user) => {
+    return users[user];
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/movies/:id" element={<MovieDetail />} />
+      </Routes>
+    </Router>
   );
 }
 
